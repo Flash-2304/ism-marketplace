014690f29aad7e81304988c801bfd92b56df76ce
FLASH-2304 <rajesh00054523@gmail.com>
2026-08-17 11:13:49 +0530

    Initialised MERN backend and frontend

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+//Imports
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config(); 
+
+//Initialize the Express app
+const app = express();
+
+//MiddleWares
+app.use(cors()); //Allows frontend to make requests
+app.use(express.json()); //Express to understand data sent in json Requests
+
+
+//Routes
+
+app.get('/', (req, res) => {
+    res.send('IIT ISM Marketplace API is running...');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
